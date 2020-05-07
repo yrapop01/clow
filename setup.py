81b6a809ebb1e15e48c547ad31328bfd1b765913
Yu Rapoport <yu.rapoport@gmail.com>
2020-05-08 01:40:20 +0300

    build & upload dist

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 from setuptools import setup
+import os
 
 setup(
     name = 'clow',
     packages = ['clow'],
     version = '0.5',
-    license='MIT',
+    license = 'MIT',
     description = 'Clow Language',
     author = 'Yuri Rapoport',
     author_email = 'yuri.rapoport@gmail.com',
@@ -12,11 +13,11 @@ setup(
     download_url = 'https://github.com/yrapop01/clow/archive/v_05.tar.gz',
     keywords = ['STATIC', 'TYPES', 'TYPING', 'INFERENCE'],
     install_requires = ['typehint'],
-    classifiers=[
+    classifiers = [
       'Development Status :: 3 - Alpha',      # "3 - Alpha", "4 - Beta" or "5 - Production/Stable"
       'Intended Audience :: Developers', 
       'Topic :: Software Development :: Build Tools',
       'License :: OSI Approved :: MIT License',
       'Programming Language :: Python :: 3',
-    ],
+    ]
 )
