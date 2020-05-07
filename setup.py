347c823e25a7b8caf216b55b331f781d6d11a69b
Yu Rapoport <yu.rapoport@gmail.com>
2020-05-08 01:29:25 +0300

    change distutils to setuptools

--- setup.py
+++ setup.py
@@ -1,20 +1,22 @@
-from distutils.core import setup
+from setuptools import setup
+
 setup(
-  name = 'clow',
-  packages = ['clow'],
-  version = '0.4',
-  license='MIT',
-  description = 'Clow Language',
-  author = 'Yuri Rapoport',
-  author_email = 'yuri.rapoport@gmail.com',
-  url = 'https://github.com/yrapop01/clow',
-  download_url = 'https://github.com/yrapop01/clow/archive/v_04.tar.gz',
-  keywords = ['STATIC', 'TYPES', 'TYPING', 'INFERENCE'],
-  classifiers=[
-    'Development Status :: 3 - Alpha',      # "3 - Alpha", "4 - Beta" or "5 - Production/Stable"
-    'Intended Audience :: Developers', 
-    'Topic :: Software Development :: Build Tools',
-    'License :: OSI Approved :: MIT License',
-    'Programming Language :: Python :: 3',
-  ],
+    name = 'clow',
+    packages = ['clow'],
+    version = '0.5',
+    license='MIT',
+    description = 'Clow Language',
+    author = 'Yuri Rapoport',
+    author_email = 'yuri.rapoport@gmail.com',
+    url = 'https://github.com/yrapop01/clow',
+    download_url = 'https://github.com/yrapop01/clow/archive/v_05.tar.gz',
+    keywords = ['STATIC', 'TYPES', 'TYPING', 'INFERENCE'],
+    install_requires = ['typehint'],
+    classifiers=[
+      'Development Status :: 3 - Alpha',      # "3 - Alpha", "4 - Beta" or "5 - Production/Stable"
+      'Intended Audience :: Developers', 
+      'Topic :: Software Development :: Build Tools',
+      'License :: OSI Approved :: MIT License',
+      'Programming Language :: Python :: 3',
+    ],
 )
